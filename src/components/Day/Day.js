bdb3be48237d968f159786b8a57f3edcd931db1d
Konstantin Tereschenkov <kdubrovsky@gmail.com>
2023-01-08 00:36:01 +0400

    Add Dish component

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -1,9 +1,15 @@
 import './Day.css';
+import Dish from "../Dish/Dish";
 
 const Day = (props) => {
     return (
         <div className="day">
             <h2 className="day-title">{props.dayName}</h2>
+            <Dish
+                title="Салат из рукколы"
+                amount="200"
+                measurement="г"
+                calories="1200"/>
         </div>);
 }
 
